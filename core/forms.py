61dd838e0a4a28f7140c39903f22e5f38e922ddf
janev <janev@LAPTOP-D63O8I88>
2026-04-10 23:56:01 +0200

    Edit user toegevoegd!

--- core/forms.py
+++ core/forms.py
@@ -62,3 +62,30 @@ class UserRegistrationForm(UserCreationForm):
         self.fields['institution'].widget.attrs.update({'class': 'form-control'})
         self.fields['password1'].widget.attrs.update({'class': 'form-control'})
         self.fields['password2'].widget.attrs.update({'class': 'form-control'})
+
+
+class UserEditForm(forms.ModelForm):
+    email = forms.EmailField(required=True, label='Email')
+    first_name = forms.CharField(max_length=30, required=True, label='Voornaam')
+    last_name = forms.CharField(max_length=30, required=True, label='Achternaam')
+    date_of_birth = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}), label='Geboortedatum')
+    function = forms.CharField(max_length=255, required=False, label='Functie')
+    description = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}), required=False, label='Beschrijving')
+    ai_knowledge_level = forms.IntegerField(min_value=1, max_value=10, required=False, label='AI kennisniveau (1-10)')
+    institution = forms.ModelChoiceField(queryset=Institution.objects.all(), required=False, label='Instelling')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'date_of_birth', 'function', 'description', 'ai_knowledge_level', 'institution')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+        self.fields['first_name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['last_name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['date_of_birth'].widget.attrs.update({'class': 'form-control'})
+        self.fields['function'].widget.attrs.update({'class': 'form-control'})
+        self.fields['description'].widget.attrs.update({'class': 'form-control'})
+        self.fields['ai_knowledge_level'].widget.attrs.update({'class': 'form-control'})
+        self.fields['institution'].widget.attrs.update({'class': 'form-control'})
